ab16f2bcb89bd33cd9dfc877501cd51fea4adf32
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-09-22 21:34:00 -0300

    refactor: move utils directory to a single utils.rs file

--- src/utils.rs
+++ src/utils.rs
@@ -1,30 +1,35 @@
 use serde::Serialize;
 use serde_json::{Map, Value};
-use crate::core::inertia_errors::InertiaErrors;
+use crate::error::InertiaError;
 
-/// Converts a struct of type `T` into a serde_json::Map.
-///
-/// ## Errors
-/// Results in `InertiaErrors` if the struct has any field that does not also implement
-/// `Serialize`.
-pub(crate) fn convert_struct_to_map<T>(s: T) -> Result<Map<String, Value>, InertiaErrors>
+pub(crate) fn convert_struct_to_map<T>(s: T) -> Result<Map<String, Value>, InertiaError>
 where T: Serialize
 {
     let struct_as_value = match serde_json::to_value(s) {
         Ok(value) => value,
         Err(_) => {
-            return Err(InertiaErrors::SerializationError("Struct is not JSON serializable.".into()))
+            return Err(InertiaError::SerializationError("Struct is not JSON serializable.".into()))
         },
     };
 
     let value_as_map = match serde_json::from_value(struct_as_value) {
         Ok(value) => value,
         Err(err) => {
-            return Err(InertiaErrors::SerializationError(format!("Failed to serialize struct as map: {}", err.to_string())));
+            return Err(InertiaError::SerializationError(format!("Failed to serialize struct as map: {}", err.to_string())));
         }
     };
 
     return Ok(value_as_map);
+}
+
+pub(crate) fn convert_struct_to_stringified_json<T>(s: T) -> Result<String, InertiaError>
+where T: Serialize
+{
+    let map = convert_struct_to_map(s)?;
+    return match serde_json::to_string(&map) {
+        Ok(json) => Ok(json),
+        Err(err) => Err(InertiaError::SerializationError(format!("Failed to serialize map to json: {}", err.to_string()))),
+    };
 }
 
 #[cfg(test)]
--- src/utils/mod.rs
+++
@@ -1,3 +0,0 @@
-mod struct_to_json_map;
-
-pub(crate) use struct_to_json_map::convert_struct_to_map;
