27f7d9e368eac879c014d4138cbf489c17c0892b
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-09-23 00:44:07 -0300

    feat: added partial component struct and inertia request type
    PartialComponent is a struct that holds the component name, the partial data and data exceptions.
    
    InertiaRequestType is an enum that holds information about the request: if it is standard, then all props is sent. If it's partial, only props with key in `only` is sent, also if there is no only, data that are on `except` won't be sent.

---
+++ src/req_type.rs
@@ -0,0 +1,41 @@
+use crate::Component;
+
+#[derive(Eq, PartialEq, Debug)]
+pub(crate) struct PartialComponent {
+    pub component: Component,
+    pub only: Vec<String>,
+    pub except: Vec<String>,
+}
+
+pub(crate) enum InertiaRequestType {
+    Standard,
+    Partial(PartialComponent)
+}
+
+impl InertiaRequestType {
+    #[inline]
+    #[allow(unused)]
+    pub fn is_standard(&self) -> bool { matches!(*self, InertiaRequestType::Standard) }
+
+    #[inline]
+    #[allow(unused)]
+    pub fn is_partial(&self) -> bool { !self.is_standard() }
+
+    #[inline]
+    pub fn unwrap_partial(self) -> PartialComponent {
+        match self {
+            InertiaRequestType::Standard => {
+                panic!("called `InertiaRequestType::unwrap_partial()` on an `Standard` request type value.");
+            },
+            InertiaRequestType::Partial(reqs) => reqs,
+        }
+    }
+
+    #[inline]
+    pub fn partials(&self) -> Option<&PartialComponent> {
+        match self {
+            InertiaRequestType::Partial(reqs) => Some(reqs),
+            InertiaRequestType::Standard => None,
+        }
+    }
+}
