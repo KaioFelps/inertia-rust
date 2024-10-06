302c94a00b1d76ca97b0671c303f9d309ff3f9b9
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-10-05 22:42:34 -0300

    test: actix provider

---
+++ tests/common/template_resolver.rs
@@ -0,0 +1,80 @@
+use std::path::Path;
+use inertia_rust::{InertiaError, TemplateResolverOutput, ViewData};
+
+
+pub const EXPECTED_RENDER: &'static str = r#"
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+</head>
+<body>
+    <div id="app" data-page={"component":"Index","props":{},"url":"/","version":"v1.0.0"}></div>
+</body>
+</html>
+"#;
+
+pub const EXPECTED_RENDER_W_PROPS: &'static str = "
+<!doctype html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">
+    \t\t
+</head>
+<body>
+    <div id=\"app\" data-page={\"component\":\"Index\",\"props\":{\"user\":\"John Doe\"},\"url\":\"/withprops\",\"version\":\"v1.0.0\"}></div>
+</body>
+</html>";
+
+
+async fn _mocked_resolver<'lf>(template_path: &str, view_data: ViewData) -> Result<String, InertiaError> {
+    let path = Path::new(template_path);
+
+    let read_file = tokio::fs::read(&path).await;
+
+    if read_file.is_err() {
+        return Err(InertiaError::SsrError(format!(
+            "Failed to open root layout at {}: {:#}",
+            path.to_str().unwrap(),
+            read_file.unwrap_err()
+        )));
+    }
+
+    let data = read_file.unwrap();
+
+    let mut html = match String::from_utf8(data) {
+        Err(err) => return Err(InertiaError::SsrError(format!("Failed to read file contents: {err:?}"))),
+        Ok(html) => html,
+    };
+
+    match view_data.ssr_page {
+        Some(ssr) => {
+            html = html.replace("%-inertia_body-%", ssr.get_body());
+            html = html.replace("%-inertia_head-%", ssr.get_head());
+        },
+        None => {
+            let stringified_page: Result<String, serde_json::Error> = serde_json::to_string(&view_data.page);
+
+            if stringified_page.is_err() {
+                return Err(InertiaError::SerializationError(format!("Failed to serialize view_data.page: {:?}", &view_data.page)));
+            }
+
+            let stringified_page = stringified_page.unwrap();
+
+            let container = format!("<div id=\"app\" data-page={stringified_page}></div>", );
+            html = html.replace("%-inertia_body-%", &container);
+            html = html.replace("%-inertia_head-%", "");
+        }
+    }
+
+    return Ok(html);
+}
+
+pub fn mocked_resolver<'lf>(template_path: &'lf str, view_data: ViewData) -> TemplateResolverOutput<'lf> {
+    Box::pin(_mocked_resolver(template_path, view_data))
+}
---
+++ tests/test_p_actix.rs
@@ -0,0 +1,106 @@
+mod common;
+
+use std::collections::HashMap;
+use actix_web::{dev::{ServiceFactory, ServiceRequest, ServiceResponse}, get, App, HttpRequest, HttpResponse};
+use common::template_resolver::{mocked_resolver, EXPECTED_RENDER, EXPECTED_RENDER_W_PROPS};
+use inertia_rust::{Component, Inertia, InertiaProp, InertiaProps, InertiaVersion};
+
+fn super_trim(text: String) -> String {
+    text.trim()
+        .replace("\r\n", "")
+        .replace("\n", "")
+        .replace("\t", "")
+}
+
+// region: --- Service
+
+#[get("/")]
+async fn home(req: HttpRequest) -> HttpResponse {
+    let response = inertia_rust::render(&req, Component("Index".into())).await;
+    if response.is_ok() {
+        return response.unwrap();
+    }
+
+    let err = response.unwrap_err();
+    println!("{:#?}", err);
+    return HttpResponse::InternalServerError().finish();
+}
+
+#[get("/withprops")]
+async fn with_props(req: HttpRequest) -> HttpResponse {
+    let mut props: InertiaProps = HashMap::new();
+    props.insert("user".to_string(), InertiaProp::Always("John Doe".into()));
+
+    let response = inertia_rust::render_with_props(
+        &req,
+        Component("Index".into()),
+        props,
+    ).await;
+    if response.is_ok() { return response.unwrap(); }
+
+    let err = response.unwrap_err();
+    println!("{:#?}", err);
+    return HttpResponse::InternalServerError().finish();
+}
+
+async fn generate_actix_app() -> App<impl ServiceFactory<
+    ServiceRequest,
+    Config = (),
+    Response = ServiceResponse,
+    Error = actix_web::Error,
+    InitError = ()
+>> {
+    let inertia = Inertia::new(
+        "https://inertiajs.com",
+        InertiaVersion::Literal("v1.0.0".into()),
+        // "tests/common/root_layout.html",
+        "tests/common/root_layout.html",
+        &mocked_resolver
+    );
+
+    App::new()
+        .app_data(inertia)
+        .service(home)
+        .service(with_props)
+}
+
+// endregion: --- Service
+
+// region: --- Tests
+
+
+#[tokio::test]
+async fn test_render() {
+    let app =
+        actix_web::test::init_service(generate_actix_app().await).await;
+
+    let req = actix_web::test::TestRequest::get().uri("/").to_request();
+    let resp = actix_web::test::call_service(&app, req).await;
+    
+    assert_eq!(200u16, resp.status().as_u16());
+
+    let body = resp.into_body();
+    let body_bytes = actix_web::body::to_bytes(body).await.unwrap();
+    let html_body = String::from_utf8(body_bytes.to_vec()).unwrap();
+
+    assert_eq!(super_trim(EXPECTED_RENDER.into()), super_trim(html_body));
+}
+
+#[tokio::test]
+async fn test_render_with_props() {
+    let app =
+        actix_web::test::init_service(generate_actix_app().await).await;
+
+    let req = actix_web::test::TestRequest::get().uri("/withprops").to_request();
+    let resp = actix_web::test::call_service(&app, req).await;
+
+    assert_eq!(200u16, resp.status().as_u16());
+
+    let body = resp.into_body();
+    let body_bytes = actix_web::body::to_bytes(body).await.unwrap();
+    let html_body = String::from_utf8(body_bytes.to_vec()).unwrap();
+
+    assert_eq!(super_trim(EXPECTED_RENDER_W_PROPS.into()), super_trim(html_body));
+}
+
+// endregion: --- Tests
