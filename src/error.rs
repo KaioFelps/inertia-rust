7c316b3adbbbe92bf2077d9dadf7731bdba47666
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-10-25 23:34:33 -0300

    feat: add implementations to InertiaError enum Added Display and Error traits impls. Added `get_cause` and `to_io_error` methods.

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use std::{error::Error, fmt, io};
+
 use crate::node_process::NodeJsError;
 
 #[derive(Debug)]
@@ -6,4 +8,27 @@ pub enum InertiaError {
     HeaderError(String),
     SsrError(String),
     NodeJsError(NodeJsError),
+}
+
+impl fmt::Display for InertiaError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Inertia Error: {}", self.get_cause())
+    }
+}
+
+impl Error for InertiaError {}
+
+impl InertiaError {
+    pub fn get_cause(&self) -> String {
+        match self {
+            InertiaError::HeaderError(err) => err.clone(),
+            InertiaError::NodeJsError(node_err) => format!("{} ({})", node_err.get_cause(), node_err.get_description()),
+            InertiaError::SerializationError(err) => err.clone(),
+            InertiaError::SsrError(err) => err.clone(),
+        }
+    }
+
+    pub fn to_io_error(self) -> io::Error {
+        io::Error::new(io::ErrorKind::Other, self.get_cause())
+    }
 }
