49970a5926027f0eca0b81770bef3c630016bf4d
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-12-31 03:47:26 -0300

    fix: `BasicViteResolver` export in `template_resolvers/mod.rs` file

--- src/features/template_resolvers/mod.rs
+++ src/features/template_resolvers/mod.rs
@@ -2,4 +2,4 @@
 mod basic_vite_resolver;
 
 #[cfg(feature = "basic-vite-resolver")]
-pub use basic_vite_resolver::template_resolver as basic_vite_resolver;
+pub use basic_vite_resolver::BasicViteResolver;
