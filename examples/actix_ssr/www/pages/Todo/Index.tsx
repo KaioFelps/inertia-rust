d24c6492ab24a088cec45f409bd651b53d1947dc
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-02 17:46:26 -0300

    examples: chore: add an Merge Prop `reset` example to the Todo page

--- examples/actix_ssr/www/pages/Todo/Index.tsx
+++ examples/actix_ssr/www/pages/Todo/Index.tsx
@@ -1,4 +1,4 @@
-import { Deferred, router, usePage } from "@inertiajs/react";
+import { Deferred, Head, router, usePage } from "@inertiajs/react";
 import { useCallback } from "react";
 
 type Task = {
@@ -16,31 +16,58 @@ export default function Todo() {
     const page = usePage<TodoProps>().props.page;
     
     const handleLoadMoreTasks = useCallback(() => {
-        router.reload(
-            {data: { page: page + 1 },
+        router.reload({
+            data: { page: page + 1 },
             only: ["page", "tasks"],
         })
     }, [page]);
 
+    const handleLoadOnlyCurrentTasks = useCallback(() => {
+        router.reload({
+            data: { page },
+            only: ["tasks"],
+            reset: [ "tasks" ]
+        })
+    }, [page]);
+
     return (
-        <main className="w-full max-w-[calc(100%_-_96px)] mx-auto h-full flex flex-col justify-center items-center">
-            <h1 className="text-6xl font-black text-center mb-5">To-do Tasks</h1>
+        <>
+            <Head>
+                <title>inertia-rust to-do!</title>
+                <meta name="description" content="The dumbiest to-do list you've ever seen!" />
+            </Head>
+            <main className="w-full max-w-[calc(100%_-_96px)] mx-auto h-full flex flex-col justify-center items-center">
+                <h1 className="text-6xl font-black text-center mb-5">To-do Tasks</h1>
 
-            <Deferred fallback="Loading tasks..." data="tasks"  >
-                <TasksList />
-            </Deferred>
+                <Deferred fallback="Loading tasks..." data="tasks"  >
+                    <TasksList />
+                </Deferred>
 
-            <button
-                className="
-                    my-6 p-7 py-4 rounded-xl bg-purple-700 hover:bg-purple-800 active:bg-purple-900
-                    transition-all duration-100 ring-0 ring-purple-600/25 focus:ring-8 outline-none
-                    select-none font-medium text-xl cursor-default
-                "
-                onClick={handleLoadMoreTasks}
-            >
-                Load few more!
-            </button>
-        </main>
+                <div className="my-6 flex gap-2 items-center justify-center">
+                    <button
+                        className="
+                            p-7 py-4 rounded-xl bg-purple-700 hover:bg-purple-800 active:bg-purple-900
+                            transition-all duration-100 ring-0 ring-purple-600/25 focus:ring-8 outline-none
+                            select-none font-medium text-xl cursor-default
+                        "
+                        onClick={handleLoadMoreTasks}
+                    >
+                        Load few more!
+                    </button>
+
+                    <button
+                        className="
+                            p-7 py-4 rounded-xl bg-purple-500/20 hover:bg-purple-600/20 active:bg-purple-800/20
+                            transition-all duration-100 ring-0 ring-purple-400/25 focus:ring-8 outline-none
+                            select-none font-medium text-xl cursor-default
+                        "
+                        onClick={handleLoadOnlyCurrentTasks}
+                    >
+                        Show only the current page! 😡
+                    </button>
+                </div>
+            </main>
+        </>
     )
 }
 
@@ -49,7 +76,7 @@ function TasksList() {
 
     return (
         <div className="flex flex-col gap-3">
-            {(tasks ?? []).map(task => (
+            {(tasks).map(task => (
                 <div
                     data-active={task.done} key={task.title}
                     className="
