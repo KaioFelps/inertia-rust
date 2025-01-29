e5bacf13d1b6a4511c3e6d9c2ec72dbfbccb52e1
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-29 00:17:36 -0300

    feat: add `ViteHBSTemplateResolver` + unit tests

--- src/features/template_resolvers/mod.rs
+++ src/features/template_resolvers/mod.rs
@@ -3,3 +3,9 @@ mod vite_template_resolver;
 
 #[cfg(feature = "vite-template-resolver")]
 pub use vite_template_resolver::ViteTemplateResolver;
+
+#[cfg(feature = "vite-hbs-template-resolver")]
+mod vite_hbs_template_resolver;
+
+#[cfg(feature = "vite-hbs-template-resolver")]
+pub use vite_hbs_template_resolver::ViteHBSTemplateResolver;
